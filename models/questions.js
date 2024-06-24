c6d5a9b1f0fa25f1fe854e1e4ae53cfabf7b225b
ankitapriya108 <139537048+ankitapriya108@users.noreply.github.com>
2024-06-24 23:45:48 +0530

    updated

--- models/questions.js
+++ models/questions.js
@@ -1,31 +1,19 @@
 import mongoose from "mongoose";
 
 const questionSchema = new mongoose.Schema({
-    // category: {
-    //     type: String,
-    //     enum: ['Sports', 'Movies', 'General'],
-    //     required: true
-    // },
-    // difficultyLevel: {
-    //     type: String,
-    //     enum: ['easy', 'medium', 'hard'],
-    //     required: true,
-    //     index: true
-    // },
+    category: {
+        type: String,
+        enum: ['Sports', 'Movies', 'General'],
+        required: true
+    },
+    difficultyLevel: {
+        type: String,
+        enum: ['easy', 'medium', 'hard'],
+        required: true,
+        index: true
+    },
     
     questionArray: [
-        category: {
-            type: String,
-            enum: ['Sports', 'Movies', 'General'],
-            required: true
-        },
-        difficultyLevel: {
-            type: String,
-            enum: ['easy', 'medium', 'hard'],
-            required: true,
-            index: true
-        },
-        
         {
             que: {
                 type: String,
